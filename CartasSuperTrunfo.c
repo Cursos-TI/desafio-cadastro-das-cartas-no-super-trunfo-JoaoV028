f8fe2ede2ddda6cd2488ef88452a956f40cb3281
João Victor <123263805+JoaoV028@users.noreply.github.com>
2025-01-11 22:10:46 +0000

    Estrutura basica do codigo, primeiro teste

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,7 @@
 //Teste larissa
 
 int main() {
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -17,6 +18,19 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    
+    char codigo[3];
+    char nome;
+    int pupulacao;
+    double area;
+    double pib;
+    int ponto_t; //pontos turísticos 
+
+    printf ("Digite o codigo da cidade: \n");
+    scanf ("%3s", &codigo);
+
+    
+   
 
     return 0;
 }
