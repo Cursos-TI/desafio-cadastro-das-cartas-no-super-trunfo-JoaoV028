24f2285b3b63158c8c698a7f6fba033b3e569a4b
João Victor <123263805+JoaoV028@users.noreply.github.com>
2025-01-12 19:57:53 +0000

    Criação do resto do código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,18 +19,48 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     
-    char codigo[3];
-    char nome;
-    int pupulacao;
-    double area;
-    double pib;
-    int ponto_t; //pontos turísticos 
 
-    printf ("Digite o codigo da cidade: \n");
-    scanf ("%3s", &codigo);
+    //Criação das Variáveis 
+    char codigo[4]; //Código da Cidade
+    char nome[26];  //Nome da Cidade
+    int populacao;  //População da Cidade
+    double area;    //Area da Cidade
+    double pib;     //PIB da Cidade
+    int ponto_t;    //pontos turísticos 
 
     
-   
+    //Entrada do Usuário
+
+    printf ("Digite o codigo da cidade (A01,C03...): \n");
+    scanf ("%3s", codigo);  //Código
+
+    printf ("\nDigite o nome da cidade (até 25 caracteres): ");
+    scanf  ("%25s", nome);  //Nome
+
+    printf ("Digite a populaçao de %s (Habitantes): ", nome);
+    scanf ("%d", &populacao);   //População
+
+    printf ("\nDigite a área de %s(em km², exemplo: 1250.00): ", nome);
+    scanf ("%lf", &area);   //Área 
+
+    printf ("Digite o PIB de %s: (em bilhões, exemplo: 350.93)", nome);
+    scanf ("%lf",&pib); //PIB
+
+    printf ("Número de pontos turísticos em %s: ", nome);
+    scanf ("%d", &ponto_t); //Pontos turísticos 
+
+
+        //Saida de Dados
+        printf ("\n========================");
+        printf ("DADOS DA CIDADE");
+        printf ("========================\n");
+        printf ("\nCódigo da cidade: %s.", codigo);
+        printf ("\nNome da cidade: %s", nome);
+        printf ("\nPopulação de %s: %d", nome, populacao);
+        printf ("\nÁrea aproximada de %s: %.2lfkm²", nome, area);
+        printf ("\nPIB de %s: %.2f", nome, pib);
+        printf ("\nPontos turísticos em %s: %d\n", nome , ponto_t);
+    
 
     return 0;
 }
